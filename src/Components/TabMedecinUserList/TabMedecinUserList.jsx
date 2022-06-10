b05059ca0262f13b6461a9bb63bcf922952e3e9a
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-06-10 15:05:28 +0200

    t

--- src/Components/TabMedecinUserList/TabMedecinUserList.jsx
+++ src/Components/TabMedecinUserList/TabMedecinUserList.jsx
@@ -13,7 +13,7 @@ const TabMedecinUserList = ({ users }) => {
   };
   const getUserList = users.flatMap((user) => {
     const registerDate = new Date(user.registerDate);
-    if (user.role !== "Doctor") {
+    if (user.role === "User") {
       return (
         <tr key={user.id}>
           <td> {user.firstname} </td>
